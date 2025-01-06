82978ded757e2343a49c1522f2db6adbeb1bcc1f
kabin007 <ghimirekabin060@gmail.com>
2025-01-06 21:47:53 +0545

    made some changes

--- backend/routes/businessRoutes.js
+++ backend/routes/businessRoutes.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const businessController = require('../controllers/businessController');
-import Business from '../models/Business';
 const { getBusinessesByOwnerId } = require('../controllers/businessController');
 
 // Create a new business
